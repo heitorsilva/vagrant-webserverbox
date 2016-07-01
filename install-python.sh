e541a35962c69cb8215ad824101d352ac758ebcb
Heitor Silva <heitor-silva@hotmail.com>
2016-07-01 14:31:17 -0300

    Fixing python install script

--- install-python.sh
+++ install-python.sh
@@ -8,5 +8,7 @@ cd ~/.pyenv && src/configure && make -C src
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
 exec $SHELL
 pyenv install 3.4.0
+pyenv rehash
