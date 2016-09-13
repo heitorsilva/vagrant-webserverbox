ff5ad776eebc62c139dc385ce013d304ba9161f6
Heitor Silva <heitor-silva@hotmail.com>
2016-09-13 00:37:31 -0300

    Including php7.0-zip package

--- install-php.sh
+++ install-php.sh
@@ -4,7 +4,7 @@ sudo mv /tmp/dotdeb.list /etc/apt/sources.list.d/dotdeb.list
 wget https://www.dotdeb.org/dotdeb.gpg
 sudo apt-key add dotdeb.gpg
 rm dotdeb.gpg
-sudo apt-get update && sudo apt-get install php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-imagick php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-readline php7.0-xdebug php7.0-xmlrpc pkg-php-tools php-pear dh-php -y
+sudo apt-get update && sudo apt-get install php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-imagick php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-readline php7.0-xdebug php7.0-xmlrpc php7.0-zip pkg-php-tools php-pear dh-php -y
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
